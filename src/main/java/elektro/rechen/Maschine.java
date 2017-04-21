448d5c50accbc09556e534c6719f7aefb8f7c37a
haluong <haluong89@gmail.com>
2017-04-21 15:02:17 +0200

    Ich bin kein Robot

--- src/main/java/elektro/rechen/Maschine.java
+++ src/main/java/elektro/rechen/Maschine.java
@@ -4,6 +4,7 @@ public class Maschine {
 
     public static void main(String[] args) {
         System.out.println("Ich hab was geändert.");
+        System.out.println("Ich bin kein Robot!");
 	    Rahmen rahmen = new Rahmen();
         rahmen.initGUI();
     }

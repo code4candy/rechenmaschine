7a716b900e3497bef7b95c5d06f39e49e59be6f4
Suzanne Kaupp <s0548825@htw-berlin.de>
2017-04-21 14:01:38 +0200

    corrects output

--- src/main/java/elektro/rechen/Maschine.java
+++ src/main/java/elektro/rechen/Maschine.java
@@ -3,7 +3,7 @@ package elektro.rechen;
 public class Maschine {
 
     public static void main(String[] args) {
-        System.out.println("Ich hab was geädert.");
+        System.out.println("Ich hab was geändert.");
 	    Rahmen rahmen = new Rahmen();
         rahmen.initGUI();
     }

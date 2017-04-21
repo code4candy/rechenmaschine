0d6a86a2571ae30e388e9d4f268b7196fac85929
Suzanne Kaupp <s0548825@htw-berlin.de>
2017-04-21 14:59:18 +0200

    adds output

--- src/main/java/elektro/rechen/Maschine.java
+++ src/main/java/elektro/rechen/Maschine.java
@@ -4,7 +4,9 @@ public class Maschine {
 
     public static void main(String[] args) {
         System.out.println("Ich hab was geändert.");
-	    Rahmen rahmen = new Rahmen();
+        System.out.println("Ich hab noch was geändert.");
+        System.out.println("Ich hab noch mal was geändert.");
+        Rahmen rahmen = new Rahmen();
         rahmen.initGUI();
     }
 }

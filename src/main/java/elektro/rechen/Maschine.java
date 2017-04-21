3b585c2ded46f81d22e9b822492e25a2852bb0b4
Flobella <Flobella@Flobellas-MacBook-Pro.local>
2017-04-21 12:44:27 +0200

    first commit

--- src/main/java/elektro/rechen/Maschine.java
+++ src/main/java/elektro/rechen/Maschine.java
@@ -3,6 +3,7 @@ package elektro.rechen;
 public class Maschine {
 
     public static void main(String[] args) {
+        System.out.println("Ich hab was geädert.");
 	    Rahmen rahmen = new Rahmen();
         rahmen.initGUI();
     }
